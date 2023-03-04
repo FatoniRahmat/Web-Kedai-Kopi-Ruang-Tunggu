a1e441eeaca9ce37b12845bb18fbfb4477b8a289
FatoniRahmat <fajrifatoni@gmail.com>
2023-03-04 13:36:24 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika humberger menu di clik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik di luar sidbar untuk mengeluarkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
